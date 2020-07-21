ead8126ca2a8959eb06d083faa6b51f9e5e17ed8
PapaKamis <PapaKamis@users.noreply.github.com>
2020-07-21 18:16:17 +0300

    This is a test commit message.

--- run.py
+++ run.py
@@ -8,46 +8,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
-
-
-# sql lite
-#
-# nums = [1,2,3,4,5,6,7,8,9]
-# numGen = random.choice(nums)
-# dateNow = str(datetime.now())
-#
-# newUser = User('Kamis', 'Kamees', '29/07/1996')
-#
-# conn = sql.connect(':memory:', check_same_thread=False)
-# c = conn.cursor()
-#
-# c.execute('''CREATE TABLE User (
-#           uID INTEGER PRIMARY KEY NOT NULL,
-#           username TEXT NOT NULL,
-#           password TEXT NOT NULL,
-#           lastname TEXT NOT NULL,
-#           firstname TEXT NOT NULL,
-#           dob NUMBERIC NOT NULL,
-#           joindate NUMERIC NOT NULL
-#         )''')
-#
-#
-# c.execute('INSERT INTO User VALUES (NULL, :user, :pass, :firstN, :lastN, :dob, :joindate)',
-#         {
-#         'user': newUser.firstName + str(numGen),
-#         'pass': '123',
-#         'firstN': newUser.firstName,
-#         'lastN': newUser.lastName,
-#         'dob': newUser.dob,
-#         'joindate': dateNow[0:10]
-#         })
-#
-#
-#
-# c.execute('SELECT * FROM User WHERE uID = 1') #, (newUser.firstName, ))
-# getUser = c.fetchall()
-# # print('--')
-# # print(c.fetchall())
-# # print('--')
-
+# this is an update test
