5f997a4eb544e3b79a53df0c97ab0d0e6a7fd2a7
PapaKamis <PapaKamis@users.noreply.github.com>
2020-07-22 14:21:26 +0300

    Search page added.

--- haray/Main/forms.py
+++ haray/Main/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 
 class ViewProduct(FlaskForm):
     submit = SubmitField('View Product')
 
 class Search(FlaskForm):
-    search = StringField('Type to search...')
+    # category = SelectField('Category', choices={('Select Category'), ('Electronics'), ('Sports')})
+    # price = SelectField('Price', choices={('Under 100', 100), ('Under 200', 200), ('Under 300', 300)}) #, ('Under 400'), ('Under 500'), ('Under 600'), ('Under 1000')})
+    search = StringField('Search for: ')
     submit = SubmitField('Search')
--- haray/Main/routes.py
+++ haray/Main/routes.py
@@ -12,38 +12,36 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-    # form = ViewProduct()
     page = request.args.get('page', 1, type=int)
-    # getProducts = Product.query.join(User, Product.user_id == User.user_id).all()
-# 'SELECT * FROM Product as p, User as u WHERE p.user_id = u.user_id'
+
     getProducts = Product.query.join(User, Product.user_id == User.user_id) \
         .filter(Product.sold == 0)\
         .order_by(Product.date_posted.desc()).paginate(page=page, per_page=7)
-
-    # getSeller = getProducts.user_id
 
     img_location = url_for('static', filename='product_pics/')
 
     return render_template('home.html', title='Home Page', getProducts=getProducts, img_location=img_location) #,  form=form,)
 
 
-@main.route('/searchresult/<string:search>', methods=['GET', 'POST'])
-def searchresults(search):
+@main.route('/search', methods=['GET', 'POST'])
+def searchresults():
     form = Search()
-    page = request.args.get('page', 1, type=int)
+    # page = request.args.get('page', 1, type=int)
 
-    searchresult = f'%{search}%'
+    getProducts = []
 
+    if form.is_submitted():
+        searchfor = f'%{form.search.data}%'
+        getProducts = Product.query.join(User, Product.user_id == User.user_id) \
+                .filter(and_(Product.sold == 0,
+                             Product.locked == 0,
+                             Product.productname.like(searchfor))) \
+                .order_by(Product.date_posted.desc()).all()
 
-    getProducts = Product.query.join(User, Product.user_id == User.user_id) \
-        .filter(and_(Product.sold == 0,
-                     Product.locked == 0,
-                     Product.productname.like(searchresult)))\
-        .order_by(Product.date_posted.desc()).paginate(page=page, per_page=5)
-
-    # getSeller = getProducts.user_id
 
     img_location = url_for('static', filename='product_pics/')
+        # return render_template('searchresult.html', title='Search results...', getProducts=getProducts, form=form,
+        #                    img_location=img_location)
 
-    return render_template('searchresult.html', title='Search results...', getProducts=getProducts, form=form,
-                           img_location=img_location)
+
+    return render_template('searchresult.html', title='Search results...', getProducts=getProducts, form=form, img_location=img_location)
